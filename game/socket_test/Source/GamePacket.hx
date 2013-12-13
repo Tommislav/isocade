2f3f76d4fd1dac557b56f2f43cdbce3801c57a26
nilstastic <nils@me03.se>
2013-12-13 19:57:20 +0100

    socket server now handles connection errors gracefully

--- game/socket_test/Source/GamePacket.hx
+++ game/socket_test/Source/GamePacket.hx
@@ -2,10 +2,29 @@ class GamePacket
 {
 	public var id:Int;
 	public var keycode:Int;
+	//public inline static var GP_MOVE = "GP_KEY";
 	public function new(id:Int)
 	{
 		this.id = id;
 	}
 	
-	public inline static var GP_MOVE = "GP_KEY";
+	public static function Parse(data:String) : GamePacket
+	{
+		var packetSegments = data.split('|');
+		var dataSegments = packetSegments[0].split(':');
+		var id = Std.parseInt(dataSegments[0]);
+        var keycode:Int = Std.parseInt(dataSegments[1]);
+		
+		var packet = new GamePacket(id);
+		packet.keycode = keycode;
+		
+		return packet;
+	}
+	
+	public static function Serialize(packet:GamePacket):String 
+	{
+		return packet.id+":"+ packet.keycode + "|";
+	}
+	
+	
 }
--- server/server.py
+++ server/server.py
@@ -18,37 +18,39 @@ clients = []
 
 # removes client and sends a message to all connected clients
 def remove_client(connection, client_id):
-    print(len(clients))
-    clients.remove(client_id)
+    del(clients[int(client_id)])
     for c in clients:
         c.sendall((client_id+":-1|").encode())
+
+def debug_output(data):
+    encoded_data = (data.decode('UTF-8'))
+    print(encoded_data)
+    print(len(clients))
 
 # client connection handler
 def client_thread(connection):
     # send the client id to the connecting client
     client_id = str(clients.index(connection))
-    conn.send((client_id+":0|").encode())
+    connection.send((client_id+":0|").encode())
 
     #receive data
     while True:
         try:
             data = connection.recv(1024)
-        except:
-            remove_client(connection, client_id)
 
-        if not data:
+            if not data:
+                break
+
+            debug_output(data)
+
+            #broadcast message to all clients
+            for c in clients:
+                c.sendall(data)
+        except:
             break
 
-        #debug purposes only
-        encoded_data = (data.decode('UTF-8'))
-        print(encoded_data)
-        print(len(clients))
-
-        #broadcast message to all clients
-        for c in clients:
-            c.sendall(data)
     #clean up
-    conn.close()
+    connection.close()
     remove_client(connection, client_id)
 
 # accept loop
