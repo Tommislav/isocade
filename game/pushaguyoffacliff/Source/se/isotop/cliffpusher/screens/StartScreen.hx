d7a7226980c44bf8e9a7b7b869e787e29340ebf6
Markus Kruusmägi <markus.kruusmagi@isotop.se>
2014-02-13 15:46:56 +0100

    Start and help screens with dummy-button images

--- game/pushaguyoffacliff/Source/se/isotop/cliffpusher/screens/StartScreen.hx
+++ game/pushaguyoffacliff/Source/se/isotop/cliffpusher/screens/StartScreen.hx
@@ -1,5 +1,7 @@
 package se.isotop.cliffpusher.screens;
 
+import flash.events.MouseEvent;
+import com.haxepunk.graphics.Image;
 import com.haxepunk.Scene;
 import com.haxepunk.HXP;
 import com.haxepunk.utils.Input;
@@ -7,12 +9,17 @@ import com.haxepunk.utils.Key;
 import com.haxepunk.graphics.Text;
 import com.haxepunk.Entity;
 import se.isotop.cliffpusher.GameScene;
+import se.isotop.cliffpusher.screens.HelpScreen;
+import com.haxepunk.utils.Touch;
 import flash.events.KeyboardEvent;
 
 class StartScreen extends Scene {
-	
+    var startButton:Entity;
+    var helpButton:Entity;
+
     public function new() {
         super();
+
     }
 
     override public function begin() {
@@ -20,14 +27,35 @@ class StartScreen extends Scene {
 
         trace("HXP screen" + HXP.width);
 
-        var titleText:Text = new Text("Press X to Start");
-        titleText.color = 0x000000;
-        titleText.size = 24;
-        var textEntity:Entity = new Entity(0,0,titleText);
-        textEntity.x = (HXP.width/2)-(titleText.width/2);
-        textEntity.y = (HXP.height/2)-(titleText.height/2);
-        add(textEntity);
+//        HXP.stage.addEventListener(MouseEvent.CLICK, onClick);
 
+        var startButtonImage:Image = new Image("assets/start_button.png");
+        startButton = new Entity(HXP.width/2-startButtonImage.width-50,HXP.height/2,startButtonImage);
+        startButton.type = "start_button";
+        startButton.setHitbox(startButtonImage.width,startButtonImage.height);
+        //addGraphic(new Image("start_button.png"));
+        add(startButton);
+
+        var helpButtonImage:Image = new Image("assets/help_button.png");
+
+        helpButton = new Entity(HXP.width/2+50,HXP.height/2,helpButtonImage);
+        helpButton.setHitbox(helpButtonImage.width,helpButtonImage.height);
+        helpButton.type = "help_button";
+
+        //addGraphic(new Image("help_button.png"));
+
+        add(helpButton);
+
+
+
+//        var titleText:Text = new Text("Press X to Start\nPress H for Help screen");
+//        titleText.color = 0x000000;
+//        titleText.size = 24;
+//        var textEntity:Entity = new Entity(0,0,titleText);
+//        textEntity.x = (HXP.width/2)-(titleText.width/2);
+//        textEntity.y = (HXP.height/2)-(titleText.height/2);
+//        add(textEntity);
+//
         var splashText:Text = new Text("IsoCade adventures");
         splashText.color = 0xBB3377;
         splashText.size = 56;
@@ -38,10 +66,44 @@ class StartScreen extends Scene {
 
     }
 
+    private function onClick(e:MouseEvent) {
+        var clickX = e.stageX;
+        var clickY = e.stageY;
+
+
+
+
+        trace("Clicked: " + clickX + " : " +clickY );
+
+    }
+
+
     override public function update():Void {
         if (Input.check(Key.X)) {
             HXP.screen.color = 0x222233;
             HXP.scene = new GameScene();
         }
+        if (Input.check(Key.H)) {
+
+            HXP.scene = new HelpScreen();
+        }
+
+        if (Input.mouseReleased) {
+
+            var e:Entity = this.collidePoint("start_button",Input.mouseX,Input.mouseY);
+
+            trace(e);
+
+            if(e != null) {
+                HXP.scene = new GameScene();
+            }
+
+            if(this.collidePoint("help_button",Input.mouseX,Input.mouseY)!= null ) {
+                HXP.scene = new HelpScreen();
+            }
+        }
+
     }
+
+
 }
